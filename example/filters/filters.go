1f02ed1492e623b96b462c06412082033f87ed11
Eugene Mutai <eugenemutai@gmail.com>
2015-07-11 23:45:55 +0300

    filter.go in example now update to show an example of how to add filters

--- example/filters/filters.go
+++ example/filters/filters.go
@@ -5,19 +5,23 @@ import (
 	"net/http"
 )
 
+// This is how a dev makes a Filter instance
 var MiddleWare = Frodo.NewFilters()
 
 func init() {
 
+	// Adding Before/After filters
 	MiddleWare.Before(func(w http.ResponseWriter, r *http.Request, _ Frodo.Params) {
 		if r.Method == "GET" {
-			r.Method = "CHANGED_BY_MIDDLEWARE"
+			r.Method = "CHANGED_BY_BEFORE_MIDDLEWARE"
 		}
+		// w.Write([]byte("Middleware wrote this, so the application should exit.\n"))
 	})
 
-	MiddleWare.Filter("/page/{id}", func(w http.ResponseWriter, r *http.Request, _ Frodo.Params) {
-		if r.Method == "GET" {
-			r.Method = "CHANGED_BY_MIDDLEWARE"
+	// Adding routin filters, this applies now to "/page/{id}" route
+	MiddleWare.Filter("/page/{id}", func(w http.ResponseWriter, r *http.Request, params Frodo.Params) {
+		if params.Get("id") != "" {
+			r.Method = "CHANGED_BY_FILTER_MIDDLEWARE"
 		}
 	}, false)
 
