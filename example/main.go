307fc2deb49709e610368f7e2b11f7bce247fcf8
Eugene Mutai <eugenemutai@gmail.com>
2015-07-13 22:57:40 +0300

    updated example to test functionality

--- example/main.go
+++ example/main.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-	"github.com/kn9ts/frodo"
-	"github.com/kn9ts/frodo/example/filters"
-	"gopkg.in/unrolled/render.v1"
-	"net/http"
+    "github.com/kn9ts/frodo"
+    "github.com/kn9ts/frodo/example/filters"
+    "gopkg.in/unrolled/render.v1"
+    "net/http"
 )
 
 func main() {
-	// Get an app instance by initializing the app
-	App := Frodo.New.Application()
-	App.AddFilters(filters.MiddleWare)
-	Reponse := render.New(render.Options{})
+    // Get an app instance by initializing the app
+    App := Frodo.New.Application()
+    App.AddFilters(filters.MiddleWare)
+    Reponse := render.New(render.Options{})
 
-	App.All("/", func(w http.ResponseWriter, r *http.Request, _ *Frodo.Params) {
-		Reponse.JSON(w, http.StatusOK, r)
-	})
+    App.All("/", func(w http.ResponseWriter, r *Frodo.Request) {
+        Reponse.JSON(w, http.StatusOK, r)
+    })
 
-	App.Match(Frodo.Methods{"GET", "POST"}, "/home", func(w http.ResponseWriter, r *http.Request, _ *Frodo.Params) {
-		Reponse.JSON(w, http.StatusOK, r)
-	})
+    App.Match(Frodo.Methods{"GET", "POST"}, "/home", func(w http.ResponseWriter, r *Frodo.Request) {
+        Reponse.JSON(w, http.StatusOK, r)
+    })
 
-	App.Get("/page/{id}", func(w http.ResponseWriter, r *http.Request, params *Frodo.Params) {
-		w.Write([]byte("Hello page here, the ID passed is " + params.Get("id"))) // send data to client side
-	})
+    App.Get("/page/{id}", func(w http.ResponseWriter, r *Frodo.Request) {
+        w.Write([]byte("Hello page here, the ID passed is " + r.Param("id"))) // send data to client side
+    })
 
-	App.Post("/{some_id}/{images}", func(w http.ResponseWriter, r *http.Request, param *Frodo.Params) {
-		w.Write([]byte("Hello, to get here. You required this ID: " + param.Get("some_id"))) // send data to client side
-	})
+    App.Post("/{some_id}/{images}", func(w http.ResponseWriter, r *Frodo.Request) {
+        // send data to client side
+        w.Write([]byte("Hello, to get here. You required this ID: " + r.Param("some_id")))
+    })
 
-	App.Post("/{name}", func(w http.ResponseWriter, r *http.Request, _ *Frodo.Params) {
-		// w.Write([]byte("Hello, " + param.Get("name") + "! This is your profile page.")) // send data to client side
-		Reponse.JSON(w, http.StatusOK, r)
-	})
+    App.Post("/{name}", func(w http.ResponseWriter, r *Frodo.Request) {
+        // send data to client side
+        // w.Write([]byte("Hello, " + param.Get("name") + "! This is your profile page."))
+        Reponse.JSON(w, http.StatusOK, r)
+    })
 
-	App.Serve()
-	// App.ServeOnPort(3000)
+    App.Serve()
+    // App.ServeOnPort(3000)
 }
