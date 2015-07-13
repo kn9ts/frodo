045d7121cdff1bcd65c8603b669750f7e50a20f8
Eugene Mutai <eugenemutai@gmail.com>
2015-07-13 23:19:50 +0300

    added a bit of documentation, plus reminder

--- mw_writer.go
+++ mw_writer.go
@@ -5,19 +5,31 @@ import (
 	"net/http"
 )
 
+// MiddlewareResponseWriter is used to hijack/embed http.ResponseWriter
+// thus making it satisfy the ResponseWriter interface, we then add a written boolean property
+// to trace when a write made and exit
 type MiddlewareResponseWriter struct {
 	http.ResponseWriter
 	written bool
 }
 
+// Write writes data back the client/creates the body
 func (w *MiddlewareResponseWriter) Write(bytes []byte) (int, error) {
 	w.written = true
-	fmt.Printf("\nA write was made by Middleware: %v\n", w.written)
+	fmt.Printf("\nAn application response was written back: %v\n", w.written)
 	return w.ResponseWriter.Write(bytes)
 }
 
+// WriteHeader is in charge of building the Header file and writing it back to the client
 func (w *MiddlewareResponseWriter) WriteHeader(code int) {
 	w.written = true
-	fmt.Printf("\nA write was made by Middleware: %v\n", w.written)
+	fmt.Printf("\nHeader was wriiten back: %v\n", w.written)
 	w.ResponseWriter.WriteHeader(code)
 }
+
+/*
+   FUTURE:
+
+   If a write happens, then we can track it here and we can place our
+   AFTER middleware just before a write happens
+*/
