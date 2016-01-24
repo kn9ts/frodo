650e39db65391c11b4c787f31224f54934b6a9a1
Eugene Mutai <eugenemutai@gmail.com>
2016-01-24 14:03:13 +0300

    [U 24012016] Added ClientIP method to Request struct

--- middleware.go
+++ middleware.go
@@ -1,6 +1,9 @@
 package frodo
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // Request will help facilitate the passing of multiple handlers
 type Request struct {
@@ -42,4 +45,24 @@ func (r *Request) Next() {
 		}
 	}
 	return
+}
+
+// ClientIP implements a best effort algorithm to return the real client IP, it parses
+// X-Real-IP and X-Forwarded-For in order to work properly with reverse-proxies such us: nginx or haproxy.
+func (r *Request) ClientIP() string {
+	if true {
+		clientIP := strings.TrimSpace(r.Request.Header.Get("X-Real-Ip"))
+		if len(clientIP) > 0 {
+			return clientIP
+		}
+		clientIP = r.Request.Header.Get("X-Forwarded-For")
+		if index := strings.IndexByte(clientIP, ','); index >= 0 {
+			clientIP = clientIP[0:index]
+		}
+		clientIP = strings.TrimSpace(clientIP)
+		if len(clientIP) > 0 {
+			return clientIP
+		}
+	}
+	return strings.TrimSpace(r.Request.RemoteAddr)
 }
